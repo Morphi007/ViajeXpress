a35c2373d426e7862adbc598b1a74af2ec7e5df5
Cynthia Lisbeth Santana Asmar <cynthiasantana23@gmail.com>
2024-06-07 18:50:13 -0400

    cambios parte 2 Julio Enmanuel

---
+++ pages/servicios.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ExpressLayout } from '@/components/layout';
+
+const Servicios = () => {
+  return (
+    <ExpressLayout title="Servicios">
+      <h1>Servicios</h1>
+    </ExpressLayout>
+  );
+};
+
+export default Servicios;
