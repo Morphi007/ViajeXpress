f38ee449600184f3c49840bbe0e974ebcc6b3f7d
Cynthia Lisbeth Santana Asmar <cynthiasantana23@gmail.com>
2024-07-21 20:04:43 -0400

    metro

--- pages/viajes.tsx
+++ pages/viajes.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useRouter } from 'next/router'; // Asegúrate de instalar next/router si no lo tienes
 import { ExpressLayout } from '@/components/layout';
 
 const Viajes = () => {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push('/metro'); // Ajusta la ruta según corresponda
+  };
+
   return (
     <ExpressLayout title="Viajes">
       <h1>Aqui la pagina de viajes</h1>
+      <button onClick={handleRedirect}>Ir a Metro</button>
     </ExpressLayout>
   );
 };
