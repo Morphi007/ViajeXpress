6787a696967503da852e7d05b9c8cf2634e4dcd0
DonySCode <donycastro80@gmail.com>
2024-08-04 20:44:18 -0400

    fix: Fixed about page

--- components/principal/SegundaPagina.tsx
+++ components/principal/SegundaPagina.tsx
@@ -1,68 +1,93 @@
 import React from 'react';
-import { SiHomeassistantcommunitystore } from 'react-icons/si';
 
 export const SegundaPagina = () => {
   return (
-    <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white py-12 px-4">
-      <div className="max-w-screen-xl mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-8 animate-fadeInUp">Sobre Nosotros</h2>
-        <p className="mb-8 text-lg animate-fadeInUp delay-100">
-          En ViajeXpress, nos dedicamos a proporcionar servicios de transporte seguros y confiables. Nuestro equipo de profesionales está comprometido con la satisfacción del cliente, ofreciendo soluciones de transporte adaptadas a sus necesidades.
-        </p>
-        <div className="grid gap-4 md:grid-cols-3">
-          <div className="animate-fadeInUp delay-200">
-            <div className="w-full h-auto rounded-lg shadow-lg">
+      <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white pt-12 pb-1">
+        <div className="max-w-screen-xl mx-auto text-center">
+          <h2 className="text-5xl font-bold mb-8 animate-fadeInUp">Sobre Nosotros</h2>
+          <p className="mb-8 text-3xl animate-fadeInUp text-justify delay-100">
+            En ViajeXpress, nos dedicamos a proporcionar servicios de transporte seguros y confiables. Nuestro equipo de profesionales está comprometido con la satisfacción del cliente, ofreciendo soluciones de transporte adaptadas a sus necesidades.
+          </p>
+        </div>
+        <div className="bg-blue-900 py-12 text-center">
+          <h3 className="text-4xl font-semibold mb-8 text-white">Nuestra Misión</h3>
+          <div className="animate-fadeInUp delay-200  grid gap-4 md:grid-cols-2 max-w-screen-xl mx-auto text-center">
               <iframe
-                width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/WKtLvtRGVGc?autoplay=1&mute=1&loop=1&playlist=WKtLvtRGVGc"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                style={{ pointerEvents: 'none' }}  // Deshabilita la interacción
+                  width="88%"
+                  height="315"
+                  src="https://www.youtube.com/embed/WKtLvtRGVGc?autoplay=1&mute=1&loop=1&playlist=WKtLvtRGVGc"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  style={{pointerEvents: 'none'}}  // Deshabilita la interacción
               ></iframe>
+            <div className="text-center text-justify mt-10">
+              <p className="mt-2 text-white text-4xl">
+                Proporcionar un servicio de transporte seguro, confiable y eficiente para todos nuestros clientes.
+              </p>
             </div>
-            <h3 className="text-xl font-semibold mt-4">Nuestra Misión</h3>
-            <p className="mt-2">
-              Proporcionar un servicio de transporte seguro, confiable y eficiente para todos nuestros clientes.
-            </p>
           </div>
-          <div className="animate-fadeInUp delay-300">
-            <div className="w-full h-auto rounded-lg shadow-lg">
-              <iframe
-                width="100%"
+        </div>
+        <div className="bg-white py-12 text-center">
+          <h3 className="text-4xl font-semibold mb-8 text-black">Nuestra Visión</h3>
+          <div className="animate-fadeInUp delay-300 grid gap-4 md:grid-cols-2 max-w-screen-xl mx-auto text-center">
+            <iframe
+                width="88%"
                 height="315"
                 src="https://www.youtube.com/embed/GlAC-JmzXJ8?autoplay=1&mute=1&loop=1&playlist=GlAC-JmzXJ8"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
-                style={{ pointerEvents: 'none' }}  // Deshabilita la interacción
-              ></iframe>
+                style={{pointerEvents: 'none'}}  // Deshabilita la interacción
+            ></iframe>
+            <div className="text-center text-justify">
+              <p className="mt-2 text-black text-4xl">
+                Nuestra misión es brindar un servicio de transporte seguro, confiable y eficiente que supere las
+                expectativas de nuestros clientes.
+              </p>
             </div>
-            <h3 className="text-xl font-semibold mt-4">Nuestros Valores</h3>
-            <p className="mt-2">
-              Compromiso, responsabilidad, y excelencia en cada viaje que realizamos.
-            </p>
           </div>
-          <div className="animate-fadeInUp delay-400">
-            <div className="w-full h-auto rounded-lg shadow-lg">
-              <iframe
-                width="100%"
+        </div>
+        <div className="bg-blue-900 py-12 text-center">
+          <h3 className="text-4xl font-semibold mb-8 text-white">Nuestros Valores</h3>
+          <div className="animate-fadeInUp delay-300 grid gap-4 md:grid-cols-2 max-w-screen-xl mx-auto text-center">
+            <iframe
+                width="88%"
                 height="315"
                 src="https://www.youtube.com/embed/3RXy9Kd2N0E?autoplay=1&mute=1&loop=1&playlist=3RXy9Kd2N0E"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
-                style={{ pointerEvents: 'none' }}  // Deshabilita la interacción
-              ></iframe>
+                style={{pointerEvents: 'none'}}  // Deshabilita la interacción
+            ></iframe>
+            <div className="text-center text-justify">
+              <p className="mt-2 text-white text-4xl">
+                En ViajeXpress, valoramos el compromiso con nuestros clientes, la responsabilidad en cada uno de
+                nuestros servicios y la excelencia en todas nuestras operaciones.
+              </p>
             </div>
-            <h3 className="text-xl font-semibold mt-4">Nuestro Equipo</h3>
-            <p className="mt-2">
-              Un equipo de profesionales dedicados a proporcionar el mejor servicio de transporte.
-            </p>
+          </div>
+        </div>
+        <div className="bg-white py-12 text-center">
+          <h3 className="text-4xl font-semibold mb-8">Nuestro Equipo</h3>
+          <div className="animate-fadeInUp delay-400 grid gap-4 md:grid-cols-2 max-w-screen-xl mx-auto text-center">
+            <iframe
+                width="88%"
+                height="315"
+                src="https://www.youtube.com/embed/WKtLvtRGVGc?autoplay=1&mute=1&loop=1&playlist=WKtLvtRGVGc"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                style={{pointerEvents: 'none'}}  // Deshabilita la interacción
+            ></iframe>
+            <div className="text-center text-justify mt-14">
+              <p className="mt-2 text-3xl">
+                Un equipo de profesionales dedicados a proporcionar el mejor servicio de transporte.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
+
